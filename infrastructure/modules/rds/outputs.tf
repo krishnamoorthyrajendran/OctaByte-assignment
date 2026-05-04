72cf66899c45aed88a710e1fc5634683371e8b5e
krishnamoorthy <krishnamoorthy@pinnacleseven.com>
2026-05-04 06:43:16 +0530

    terraform infrastructure

--- infrastructure/modules/rds/outputs.tf
+++ infrastructure/modules/rds/outputs.tf
@@ -6,6 +6,6 @@ output "db_port" {
   value = aws_db_instance.this.port
 }
 
-output "db_secret_arn" {
-  value = aws_secretsmanager_secret.db_secret.arn
-}
+#output "db_secret_arn" {
+#  value = aws_secretsmanager_secret.db_secret.arn
+#}
